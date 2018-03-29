8888125b6f9616fbeb7fc97a6c816546f76e1647
Molly Clemens <mclemens@ucdavis.edu>
2018-03-28 17:26:20 -0700

    clarifying messy code, this script needs to run after the licor script, or I need to copy over the first part licor to get the variety_traits in both

--- scripts/pheno_script.R
+++ scripts/pheno_script.R
@@ -6,14 +6,15 @@ Pheno2015 <- read_csv("data/Phenodata/Pheno2015.csv")
 PhenoVarRMI <- read_csv("data/Phenodata/PhenoVarRMI.csv")
 
 #plot phenology with water use traits
+#variety_traits comes from the licor_plots script, combining the original licor data
+
 Pheno2015$variety <- gsub(pattern = " ", replacement = "_",Pheno2015$variety)
 
 pheno_joined <- full_join(Pheno2015, PhenoVarRMI, by = "variety")
-
 pheno_joined2 <- full_join(pheno_joined, variety_traits, by = "variety")
-
 pheno_joined3 <- pheno_joined2 %>% 
   filter(!is.na(mean_wue))
+
 
 ggplot() + geom_point(data = pheno_joined3, aes(x = variety, y = mean_wue, color = region))+
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
